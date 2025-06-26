d00fc8c6708a904f8b51c1751da93b523ff506a3
Leandro Miranda Fahur Machado <leandrofahur@gmail.com>
2025-06-25 20:04:40 -0700

    pydantic to handle validation

--- src/dfsm/core/fsm.py
+++ src/dfsm/core/fsm.py
@@ -1,4 +1,5 @@
 from typing import Dict, List, Set, Tuple, Optional
+from dfsm.models.fsm_model import FSMModel
 
 # TODO: Extend the FSM class to include the output function to behave as a Moore/Mealy machine.
 class FSM:
@@ -13,11 +14,22 @@ class FSM:
             initial_state: the state the machine starts in.
             (optional) final_states: the set of states the machine ends in.
         '''
-        self.states = states
-        self.alphabet = alphabet
-        self.transition_functions = transition_functions
-        self.initial_state = initial_state
-        self.final_states = final_states        
+        fsm_model = FSMModel(
+            states=states,
+            alphabet=alphabet, 
+            transition_functions=transition_functions, 
+            initial_state=initial_state, 
+            final_states=final_states
+        )
+
+        # Validate the FSM model.
+        fsm_model.validate_transitions()
+        
+        self.states = fsm_model.states
+        self.alphabet = fsm_model.alphabet
+        self.transition_functions = fsm_model.transition_functions
+        self.initial_state = fsm_model.initial_state
+        self.final_states = fsm_model.final_states        
 
     def transitions(self, state: str, input_symbol: str) -> str:
         '''
@@ -38,17 +50,17 @@ class FSM:
         except KeyError:
             raise ValueError(f"Transition ({state}, {input_symbol}) not defined")
         
-    def is_final_state(self, state: str) -> bool:
+    def is_valid_state(self, state: str) -> bool:
         '''
-        Checks if the state is a final state.
+        Checks if the state is in the set of states.
 
         Args:
             state: the current state.
 
         Returns:
-            True if the state is a final state, False otherwise.
+            True if the state is in the set of states, False otherwise.
         '''
-        pass
+        return state in self.states    
     
     def is_valid_input(self, input_symbol: str) -> bool:
         '''
@@ -59,8 +71,8 @@ class FSM:
 
         Returns:
             True if the input symbol is in the alphabet, False otherwise.
-        '''
-        pass
+        '''                      
+        return input_symbol in self.alphabet
         
     def run(self, input_sequence: List[str]) -> str:
         '''
@@ -72,30 +84,28 @@ class FSM:
         Returns: the final state.
         '''
         
-        current_state = self.initial_state        
-        for input_symbol in input_sequence:            
-            # print(f"({current_state}, {input_symbol}) -> {self.transitions(current_state, input_symbol)}")
+        current_state = self.initial_state
+        for input_symbol in input_sequence:
+            if not self.is_valid_input(input_symbol):
+                raise ValueError(f"Invalid input symbol: {input_symbol}")            
+            print(f"({current_state}, {input_symbol}) -> {self.transitions(current_state, input_symbol)}")
             current_state = self.transitions(current_state, input_symbol)            
         return current_state    
 
 
-# states = {'Locked', 'Unlocked'}
-# alphabet = {'Coin', 'Push'}
-# transition_functions = {
-#     ('Locked','Push'): 'Locked',
-#     ('Locked','Coin'): 'Unlocked',
-#     ('Unlocked','Push'): 'Locked',
-#     ('Unlocked','Coin'): 'Unlocked',
-# }
+states = {'Locked', 'Unlocked'}
+alphabet = {'Coin', 'Push'}
+transition_functions = {
+    ('Locked','Push'): 'Locked',
+    ('Locked','Coin'): 'Unlocked',
+    ('Unlocked','Push'): 'Locked',
+    ('Unlocked','Coin'): 'Unlocked',
+}
 
-# initial_state = 'Locked'
-# final_states = {'Unlocked'}
-# input_sequence = ['Push', 'Coin', 'Push', 'Push', 'Coin']
-
-
-# fsm = FSM(states, alphabet, transition_functions, initial_state, final_states)
-# fsm.run(input_sequence)
-
-# print(fsm)
+initial_state = 'Locked'
+final_states = {'Unlocked'}
+input_sequence = ['Push', 'Coin', 'Push', 'Push', 'Coin']
 
 
+fsm = FSM(states, alphabet, transition_functions, initial_state, final_states)
+fsm.run(input_sequence)
---
+++ src/dfsm/models/fsm_model.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel, Field, ValidationError
+from typing import Dict, Set, Tuple, Optional
+
+class FSMModel(BaseModel):
+    states: Set[str] = Field(...,min_length=1, description="The set of states")
+    alphabet: Set[str] = Field(...,min_length=1, description="The set of input symbols")
+    transition_functions: Dict[Tuple[str, str], str] = Field(..., description="The transition functions")
+    initial_state: str = Field(..., min_length=1, description="The initial state")
+    final_states: Optional[Set[str]] = Field(None, description="The set of final states")
+
+    def validate_transitions(self) -> None:
+        """
+        Custom validation for transition function completeness.
+
+        Args:
+            self: the FSM model.
+
+        Returns:
+            None.
+
+        Raises:
+            ValueError: If the transition function is not valid.
+        """
+        for (state, symbol), next_state in self.transition_functions.items():
+            if state not in self.states:
+                raise ValueError(f"State '{state}' in transition not in states set")
+            if next_state not in self.states:
+                raise ValueError(f"Next state '{next_state}' not in states set")
+            if symbol not in self.alphabet:
+                raise ValueError(f"Symbol '{symbol}' not in alphabet")
+        
+        if self.initial_state not in self.states:
+            raise ValueError(f"Initial state '{self.initial_state}' not in states set")
+        
+        if self.final_states:
+            for state in self.final_states:
+                if state not in self.states:
+                    raise ValueError(f"Final state '{state}' not in states set")
